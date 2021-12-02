273be25302a1c3d331c0397f09d2570add52e51e
Luna <49705655+Luunae@users.noreply.github.com>
2021-12-02 05:49:39 -0800

    Initial commit 2
    electric boogaloo

---
+++ src/day1.rs
@@ -0,0 +1,3 @@
+pub fn do_challenge_or_whatever() {
+    unimplemented!()
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod day1; // I'm going to miss scripting. This really doesn't seem set up for AoC-style challenges.
+
+fn main() {
+    day1::do_challenge_or_whatever();
+}
