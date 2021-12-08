5d6b101d727ce89994f5871bfbb4ea68cf266178
Luna <49705655+Luunae@users.noreply.github.com>
2021-12-08 07:36:58 -0800

    Refactor project style into single-file, correct all clippy warnings/errors
    EXCEPT NEEDLESS_RETURN

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,48 @@
-#![allow(unused_imports, unreachable_code, dead_code)]
+// Fuck it, big file time.
+#![allow(unreachable_code, dead_code, clippy::needless_return)]
 
-mod day01; // I'm going to miss scripting. This really doesn't seem set up for AoC-style challenges.
+const CONTENTS: &str = include_str!("../inputs/input01-1.txt");
 
 fn main() {
-    day01::challenge_01_2();
+    challenge_01_1();
+}
+
+fn get_input() -> Vec<&'static str> {
+    let list: Vec<&str> = CONTENTS.split('\n').collect();
+    return list;
+}
+
+fn convert_vec_of_str_to_vec_of_int(str_list: Vec<&str>) -> Vec<i64> {
+    let mut int_list: Vec<i64> = Vec::new();
+    for element in str_list {
+        int_list.push(element.parse().expect("not a valid number"))
+    }
+    return int_list
+}
+
+pub fn challenge_01_1() {
+    let list:Vec<i64> = convert_vec_of_str_to_vec_of_int(get_input());
+    let mut increases: usize = 0;
+    for index in 0..list.len() - 1 {
+        if list[index] < list[index + 1] {
+            increases += 1;
+        }
+    }
+    print!("{}", increases)
+}
+
+pub fn challenge_01_2() {
+    let list = get_input();
+    let mut increases: usize = 0;
+    for index in 0..list.len() - 1 {
+        if index == list.len() - 3 {
+            break;
+        }
+        if list[index].to_owned() + list[index + 1] + list[index + 2]
+            < list[index + 1].to_owned() + list[index + 2] + list[index + 3]
+        {
+            increases += 1
+        }
+    }
+    print!("{}", increases)
 }
