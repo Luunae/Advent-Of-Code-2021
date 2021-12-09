3c5c92b29d9d51178650147cb933b6ba13b1bd23
Luna <49705655+Luunae@users.noreply.github.com>
2021-12-09 01:28:21 -0800

    Day 2/1
    Added itertools, strum.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
 // Fuck it, big file time.
 #![allow(unreachable_code, dead_code, clippy::needless_return)]
+use itertools::Itertools;
+use std::str;
+use std::str::FromStr;
+use strum_macros::EnumString;
+
+type Coordinate = i64;
+
+#[derive(Clone, Copy)]
+struct Location {
+    x: Coordinate,
+    y: Coordinate,
+}
+
+#[derive(Debug, PartialEq, EnumString)]
+enum Direction {
+    #[strum(ascii_case_insensitive)]
+    Up,
+    #[strum(ascii_case_insensitive)]
+    Down,
+    #[strum(ascii_case_insensitive)]
+    Forward,
+    #[strum(ascii_case_insensitive)]
+    Backward,
+}
 
 fn main() {
-    challenge_01_2();
+    challenge_02_1();
 }
 
 fn get_input(file: &str) -> Vec<&str> {
@@ -16,6 +40,24 @@ fn convert_vec_of_str_to_vec_of_int(str_list: Vec<&str>) -> Vec<i64> {
         int_list.push(element.trim().parse().expect("not a valid number"))
     }
     return int_list;
+}
+
+fn submarine_navigate(mut position: Location, movement: &str) -> Location {
+    let (direction, magnitude): (&str, &str) = movement
+        .split(' ')
+        .collect_tuple()
+        .expect("Should have enough entries");
+    let magnitude: u32 = magnitude.parse().expect("Should be a valid number");
+    let magnitude = magnitude as Coordinate;
+
+    let direction: Direction = Direction::from_str(direction).expect("Valid Direction?");
+    match direction {
+        Direction::Up => position.y -= magnitude,
+        Direction::Down => position.y += magnitude,
+        Direction::Forward => position.x += magnitude,
+        Direction::Backward => position.x -= magnitude,
+    };
+    return position;
 }
 
 fn challenge_01_1() {
@@ -44,4 +86,14 @@ fn challenge_01_2() {
         }
     }
     print!("{}", increases)
+}
+
+fn challenge_02_1() {
+    let list = get_input(include_str!("../inputs/02.txt"));
+    let mut position: Location = Location { x: 0, y: 0 };
+    for command in list {
+        position = submarine_navigate(position, command);
+    }
+    let result = position.x * position.y;
+    println!("{}", result);
 }
